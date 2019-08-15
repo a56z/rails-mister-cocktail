ec31e9c0c2919a8030b775b9309754e68d29b815
Enrique <hhfolte@gmail.com>
2019-08-15 20:12:27 +0200

    adding debug gems

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,29 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.find(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path(@cocktail.id)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
 end
+
+
+
