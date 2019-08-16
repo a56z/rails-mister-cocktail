cb822c0da3046f5c86a85ae058eda6ee694689bc
Enrique <hhfolte@gmail.com>
2019-08-16 10:50:09 +0200

    adding controllers, models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :new, :create, :show] do
-  resources :dose, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
+  delete "doses/:id", to: "doses#destroy"
 end
